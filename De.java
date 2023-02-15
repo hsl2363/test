a0be284a1ef731fc44fb6bad23e955555fc9a1cd
hsl2363 <hshaolinv@sina.com>
2023-02-15 11:11:48 +0800

    ?

---
+++ De.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class De {
+
+    public static void main(String args[]) {
+
+        Scanner I = new Scanner(System.in);
+        int a = I.nextInt(), b = I.nextInt();
+        int c = a + b;
+        System.out.println(c);
+        I.close();
+    }
+}
